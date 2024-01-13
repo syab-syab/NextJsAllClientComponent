3e713d8365d9756f7ca74d4872449649ac00150b
syab-syab <syabsyabsyabsyab@gmail.com>
2024-01-13 23:57:03 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,103 @@
-import Image from 'next/image'
+"use client"
+
+import React, { useState } from 'react'
+// import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+
+  // inputタグの状態管理
+  const [inputValue, setInputValue] = useState<string>("")
+
+  const changeInputValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value)
+  }
+
+
+  // selectタグの状態管理
+  const [selectValue, setSelectValue] = useState<string>("")
+
+  const changeSelectValue = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    setSelectValue(e.target.value)
+  }
+
+
+  // radioボタンの状態管理
+  interface Radio {
+    label: string
+    value: string
+  }
+
+  // 選択中のラジオボタンvalue
+  const [selected, setSelected] = useState<string>("Beef")
+
+  // ラジオボタン切り替えイベント
+  const changeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelected(e.target.value)
+  }
+
+  // ラジオボタン
+  const radioButtons: Radio[] = [
+    {
+      label: "牛肉",
+      value: "Beef"
+    },
+    {
+      label: "鶏肉",
+      value: "Chicken"
+    },
+    {
+      label: "豚肉",
+      value: "Pork"
+    }
+  ]
+
+
+  // textareaの状態管理
+  const [textAreaValue, setTextAreaValue] = useState<string>("テキストエリア")
+
+  const changeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setTextAreaValue(e.target.value)
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <h1>とりあえずすべてをクライアントコンポーネントで作成する</h1>
+      <div>
+        {/* input */}
+        <p>Input's value = 「{inputValue}」</p>
+        <input type="text" value={inputValue} onChange={(e) => changeInputValue(e)}/>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        {/* select */}
+        <p>Select's value = 「{selectValue}」</p>
+        <select value={selectValue} onChange={(e) => changeSelectValue(e)}>
+          <option value="">数字を選んでください</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        {/* radio 後回し */}
+        {/* <p>Radio Button's value = 「{selected}」</p>
+        {
+          radioButtons.map(radio => {
+            <div>
+              <input type="radio" name='meat' value={radio.value} checked={radio.value === selected} onChange={(e) => changeValue(e)} />
+              <label>
+                <span>{radio.label}</span>
+              </label>
+            </div>
+          })
+        } */}
+      </div>
+      <div>
+        {/* textarea */}
+        <p>Textarea's value = 「{textAreaValue}」</p>
+        <textarea onChange={(e) => changeTextArea(e)} value={textAreaValue} />
       </div>
     </main>
   )

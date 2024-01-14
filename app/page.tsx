84a8ac4f7f11bd6b730d9d6a291fea29b062e9d4
syab-syab <syabsyabsyabsyab@gmail.com>
2024-01-14 23:58:56 +0900

    チェックボックス追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,16 @@ import React, { useState } from 'react'
 // import Image from 'next/image'
 import styles from './page.module.css'
 
-/** ラジオボタン設定 */
+/** ラジオボタンの型 */
 interface Radio {
   label: string
   value: string
+}
+
+// checkboxの型
+interface Check {
+  id: number
+  item: string
 }
 
 export default function Home() {
@@ -39,7 +45,7 @@ export default function Home() {
   // radioボタンの状態管理
   const [radioSelected, setRadioSelected] = useState("Beef");
 
-  const changeValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const changeRadioValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setRadioSelected(e.target.value)
   }
 
@@ -60,6 +66,37 @@ export default function Home() {
 
 
   // checkbox状態管理
+  // これは大体がコピペだから後でコード読んで理解する(特にhandleCheckedValueの中身)
+  const [checkedValues, setCheckedValues] = useState<string[]>([])
+
+  const handleCheckedValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (checkedValues.includes(e.target.value)) {
+      setCheckedValues(
+        checkedValues.filter(checkedValue => checkedValue !== e.target.value)
+      )
+    } else {
+      setCheckedValues([...checkedValues, e.target.value]);
+    }
+  }
+
+  const items: Check[] = [
+    {
+      id: 1,
+      item: "読書"
+    },
+    {
+      id: 2,
+      item: "スポーツ"
+    },
+    {
+      id: 3,
+      item: "旅行"
+    },
+    {
+      id: 4,
+      item: "音楽"
+    }
+  ];
 
   return (
     <main className={styles.main}>
@@ -67,12 +104,19 @@ export default function Home() {
       <div>
         {/* input */}
         <p>Input's value = 「{inputValue}」</p>
-        <input type="text" value={inputValue} onChange={(e) => changeInputValue(e)}/>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => changeInputValue(e)}
+        />
       </div>
       <div>
         {/* select */}
         <p>Select's value = 「{selectValue}」</p>
-        <select value={selectValue} onChange={(e) => changeSelectValue(e)}>
+        <select
+          value={selectValue}
+          onChange={(e) => changeSelectValue(e)}
+        >
           <option value="">数字を選んでください</option>
           <option value="1">1</option>
           <option value="2">2</option>
@@ -84,22 +128,54 @@ export default function Home() {
       <div>
         {/* textarea */}
         <p>Textarea's value = 「{textAreaValue}」</p>
-        <textarea onChange={(e) => changeTextArea(e)} value={textAreaValue} />
+        <textarea
+          onChange={(e) => changeTextArea(e)}
+          value={textAreaValue}
+        />
       </div>
       <div>
-      <div>Radio button's = 「{radioSelected}」</div>
-      {radioButtons.map(radio => {
-        return (
-          <div key={radio.value}>
-            {/* checked属性に式を定義する */}
-            <input type="radio" name="sweets" id={radio.value} value={radio.value} checked={radio.value === radioSelected} onChange={(e) => changeValue(e)}/>
-            <label htmlFor={radio.value}>
-              <span>{radio.label}</span>
-            </label>
-          </div>
-        )
-      })}
-    </div>
+        {/* radio button */}
+        <div>Radio button's = 「{radioSelected}」</div>
+        {radioButtons.map(radio => {
+          return (
+            <div key={radio.value}>
+              {/* checked属性に式を定義する */}
+              <input
+                type="radio"
+                name="sweets"
+                id={radio.value}
+                value={radio.value}
+                checked={radio.value === radioSelected}
+                onChange={(e) => changeRadioValue(e)}
+              />
+              <label htmlFor={radio.value}>
+                <span>{radio.label}</span>
+              </label>
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        {/* checkbox */}
+        <p>Check Box = 「{checkedValues}」</p>
+        {
+          items.map(item => {
+            return (
+              <div>
+                <input
+                  type="checkbox"
+                  id={item.item}
+                  value={item.item}
+                  onChange={(e) => handleCheckedValue(e)}
+                  checked={checkedValues.includes(item.item)}
+                />
+                <label htmlFor={item.item}>{item.item}</label>
+              </div>
+              
+            )
+          })
+        }
+      </div>
     </main>
   )
 }

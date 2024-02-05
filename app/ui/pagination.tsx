aa28ddd838a5942ce017cbace30f26ddf1b1d477
syab-syab <syabsyabsyabsyab@gmail.com>
2024-02-05 10:13:44 +0900

    ページネーション作成開始

---
+++ app/ui/pagination.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pagination = () => {
+  // データを取ってきて配列にする
+  // 配列の要素数を length で数える
+  // x刻みで区切る(10とか)
+  // 要素数 / x = ページ数
+  return (
+    <div>P</div>
+  )
+}
+
+export default Pagination

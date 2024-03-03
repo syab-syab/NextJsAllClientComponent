b07a684f00aec981eae68b717bace7b5eb9a2d4f
syab-syab <syabsyabsyabsyab@gmail.com>
2024-03-03 16:44:29 +0900

    使用グラフライブラリごとにルーティング完了

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,14 +1,30 @@
 // import React from 'react'
+import Link from "next/link"
 
 
-// バブルソート React Charts
-// 円(パイ, ドーナツ)グラフ Victory React-chartjs-2
-// 帯グラフ React Charts
-// レーダーチャート Victory
+// バブルチャート ReactCharts React-chartjs-2
+// 円(パイ, ドーナツ)グラフ Victory React-chartjs-2 Recharts
+// 鶏頭図 Victory React-chartjs-2 Recharts(SimpleRadialBarChart)
+// 帯グラフ ReactCharts
+// レーダーチャート Victory React-chartjs-2 ReactCharts
 // 統計地図 
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <div>
+        <Link href="react-charts">ReactCharts</Link>
+      </div>
+      <div>
+        <Link href="victory">Victory</Link>
+      </div>
+      <div>
+        <Link href="chartjs2">React-chartjs-2</Link>
+      </div>
+      <div>
+        <Link href="recharts">Recharts</Link>
+      </div>
+
+    </main>
   )
 }
 
---
+++ app/chart/victory/page.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <h1>Victory</h1>
+      </div>
+    </>
+  )
+}
+
+export default page
